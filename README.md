a545a19d8b0419d845bfcb0439673e884f6262d7
Carlton Joseph <carlton.joseph@gmail.com>
2017-08-24 14:37:16 -0700

    Add insert and update endpoints

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Created code following
 [this tutorial](https://www.youtube.com/watch?v=qqzIA1BQ_ys&t=241s).
 
 The initial commit has a fully operational server with minimal extra code.
+
+Run both servers via:
+
+  - `npm run json:server` and
+  - `npm run dev:server` and
--- schema.js
+++ schema.js
@@ -38,12 +38,6 @@ const RootQuery = new GraphQLObjectType({
           console.log(r.data);
           return r.data
         }).catch(e => {console.log('error'); return {}});
-        /*
-        for(let i=0; i < customers.length; i++) {
-          if (customers[i].id === args.id) {
-            return customers[i];
-          }
-        }*/
       }
     },
     customers: {
@@ -54,12 +48,57 @@ const RootQuery = new GraphQLObjectType({
           console.log(r.data);
           return r.data
         }).catch(e => {console.log('error'); return {}});
-        //return customers;
       }
     }
   }),
 });
 
+const mutation = new GraphQLObjectType({
+  name: 'mutation',
+  fields: {
+    addCustomer: {
+      type: CustomerType,
+      args: {
+        name: {type: new GraphQLNonNull(GraphQLString)},
+        email: {type: new GraphQLNonNull(GraphQLString)},
+        age: {type: new GraphQLNonNull(GraphQLInt)}
+      },
+      resolve(parentValue, args) {
+        return axios.post('http://localhost:3000/customers', {
+          name: args.name,
+          email: args.email,
+          age:  args.age
+        }).then(res => res.data)
+      }
+    },
+    deleteCustomer: {
+      type: CustomerType,
+      args: {
+        id: {type: new GraphQLNonNull(GraphQLString)},
+      },
+      resolve(parentValue, args) {
+        console.log('delete');
+        return axios.delete('http://localhost:3000/customers/' + args.id)
+          .then(res => res.data);
+      }
+    },
+    editCustomer: {
+      type: CustomerType,
+      args: {
+        id: {type: new GraphQLNonNull(GraphQLString)},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        age: {type: GraphQLInt}
+      },
+      resolve(parentValue, args) {
+        return axios.patch('http://localhost:3000/customers/' + args.id, args)
+          .then(res => res.data)
+      }
+    },
+  }
+});
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation
 });
